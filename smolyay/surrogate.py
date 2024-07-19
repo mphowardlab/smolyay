76f73054dab318cfdc7b9d3187c7e2f343cccdf0
mzbush <mzb0108@auburn.edu>
2024-07-19 13:42:47 -0500

    Revert "Remove flags related to fit_gradient"
    This reverts commit 8143d7761a4ecdbf088c1b9ce7d9cadf558a5f8f.

--- smolyay/surrogate.py
+++ smolyay/surrogate.py
@@ -169,6 +169,8 @@ class SetProductSurrogate(Surrogate):
         self._regularization = None
         self._index_combinations = None
         self._coefficients = None
+        self._fit_gradient_flag = False
+        self._integration_constant_flag = False
 
         self._basis_sets = basis_sets
         self.regularization = regularization
@@ -593,6 +595,7 @@ class SetProductSurrogate(Surrogate):
         else:
             self._coefficients = numpy.squeeze(self.regularization.fit(basis_matrix, data).coef_)
         self._valid_cache = True
+        self._fit_gradient_flag = True
         if not constant_x is None and not constant_y is None:
             # validate data inputs
             constant_x = numpy.array(constant_x, ndmin=2)
@@ -602,6 +605,9 @@ class SetProductSurrogate(Surrogate):
             predicted_y = self.predict(constant_x)
             integration_constant = constant_y - predicted_y
             self._coefficients[0] = integration_constant
+            self._integration_constant_flag = True
+        else:
+            self._integration_constant_flag = False
         return self
 
     @abc.abstractmethod
