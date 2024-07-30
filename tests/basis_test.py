241fde09afabd2d1ddf68e04c0da2bb734b9e5ce
mzbush <mzb0108@auburn.edu>
2024-07-30 12:37:49 -0500

    Add initialize tests for unnested sets

--- tests/basis_test.py
+++ tests/basis_test.py
@@ -804,6 +804,63 @@ def test_set_base_class_initialize_error():
             [smolyay.basis.ChebyshevFirstKind(0), smolyay.basis.ChebyshevSecondKind(0)]
         )
 
+@pytest.mark.parametrize(
+    "basis_set",
+    [
+        smolyay.basis.ChebyshevFirstKindBasisFunctionSet,
+        smolyay.basis.ChebyshevSecondKindBasisFunctionSet,
+    ],
+    ids=[
+        "ChebyshevFirstKind",
+        "ChebyshevSecondKind",
+    ],
+)
+def test_set_cheb_initialize(basis_set):
+    """Test Chebyshev function sets correctly initialize"""
+    f = basis_set(3)
+    assert f.basis_functions[0].degree == 0
+    assert f[0].degree == 0
+    assert f.basis_functions[1].degree == 1
+    assert f[1].degree == 1
+    assert f.basis_functions[2].degree == 2
+    assert f[2].degree == 2
+    assert len(f) == 3
+    assert numpy.array_equal(f.domain,[-1, 1])
+
+def test_set_trig_initialize():
+    """Test TrigonometricBasisFunctionSet correctly initializes"""
+    f = smolyay.basis.TrigonometricBasisFunctionSet(3)
+    assert f.basis_functions[0].frequency == 0
+    assert f[0].frequency == 0
+    assert f.basis_functions[1].frequency == 1
+    assert f[1].frequency == 1
+    assert f.basis_functions[2].frequency == -1
+    assert f[2].frequency == -1
+    assert len(f) == 3
+    assert numpy.array_equal(f.domain,[0, 2 * numpy.pi])
+
+@pytest.mark.parametrize(
+    "basis_set",
+    [
+        smolyay.basis.ChebyshevFirstKindBasisFunctionSet,
+        smolyay.basis.ChebyshevSecondKindBasisFunctionSet,
+        smolyay.basis.TrigonometricBasisFunctionSet,
+    ],
+    ids=[
+        "ChebyshevFirstKind",
+        "ChebyshevSecondKind",
+        "Trigonometric",
+    ],
+)
+def test_set_initialize_empty(basis_set):
+    """Test basis function sets initialize when empty"""
+    f = basis_set(0)
+    assert len(f) == 0
+    assert numpy.array_equal(f.basis_functions,[])
+    with pytest.raises(IndexError):
+        f.domain
+
+
 def test_nested_set_base_class_initialize():
     """Test NestedBasisFunctionSet correctly initializes"""
     f = smolyay.basis.NestedBasisFunctionSet([],[])
