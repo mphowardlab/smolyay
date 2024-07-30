74f93bfea32f031ecd4d009cebeb4beef9b8c047
mzbush <107584654+mzbush@users.noreply.github.com>
2024-07-30 14:21:39 -0500

    Update smolyay/basis.py
    Co-authored-by: Michael Howard <mphoward@auburn.edu>

--- smolyay/basis.py
+++ smolyay/basis.py
@@ -532,7 +532,7 @@ class BasisFunctionSet(collections.abc.Sequence):
     @property
     def domain(self):
         """numpy.ndarray: Domain of the `basis_functions`"""
-        if self._basis_functions is None or len(self._basis_functions) == 0:
+        if len(self._basis_functions) == 0:
             raise AttributeError("No basis functions to derive a domain.")
         return self._basis_functions[0].domain
 
