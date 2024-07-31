c36b12053976b6bd6e5833789c338fc74782b9b5
mzbush <mzb0108@auburn.edu>
2024-07-31 15:51:58 -0500

    Edit Ridge and Lasso input

--- smolyay/surrogate.py
+++ smolyay/surrogate.py
@@ -464,14 +464,10 @@ class SetProductSurrogate(Surrogate):
             else:
                 self._coefficients = numpy.linalg.lstsq(basis_matrix, y, rcond=None)[0]
         elif isinstance(self.regularization, L2Regularization):
-            regressor = sklearn.linear_model.Ridge(
-                fit_intercept=False, **self.regularization.__dict__
-            )
+            regressor = regressor = sklearn.linear_model.Ridge(alpha=self.regularization.alpha, fit_intercept=False)
             self._coefficients = numpy.squeeze(regressor.fit(basis_matrix, y).coef_)
         elif isinstance(self.regularization, L1Regularization):
-            regressor = sklearn.linear_model.Lasso(
-                fit_intercept=False, **self.regularization.__dict__
-            )
+            regressor = regressor = sklearn.linear_model.Lasso(alpha=self.regularization.alpha, fit_intercept=False)
             self._coefficients = numpy.squeeze(regressor.fit(basis_matrix, y).coef_)
         else:
             self._coefficients = numpy.linalg.lstsq(basis_matrix, y, rcond=None)[0]
@@ -585,14 +581,10 @@ class SetProductSurrogate(Surrogate):
         if self.regularization is None or numpy.iscomplexobj(basis_matrix):
             self._coefficients = numpy.linalg.lstsq(basis_matrix, data, rcond=None)[0]
         elif isinstance(self.regularization, L2Regularization):
-            regressor = sklearn.linear_model.Ridge(
-                fit_intercept=False, **self.regularization.__dict__
-            )
+            regressor = sklearn.linear_model.Ridge(alpha=self.regularization.alpha, fit_intercept=False)
             self._coefficients = numpy.squeeze(regressor.fit(basis_matrix, data).coef_)
         elif isinstance(self.regularization, L1Regularization):
-            regressor = sklearn.linear_model.Lasso(
-                fit_intercept=False, **self.regularization.__dict__
-            )
+            regressor = sklearn.linear_model.Lasso(alpha=self.regularization.alpha, fit_intercept=False)
             self._coefficients = numpy.squeeze(regressor.fit(basis_matrix, data).coef_)
         else:
             self._coefficients = numpy.linalg.lstsq(basis_matrix, data, rcond=None)[0]
