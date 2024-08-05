04dc9d74bf6b2f9f95cb028356596a6cf4275b18
mzbush <107584654+mzbush@users.noreply.github.com>
2024-08-05 12:20:13 -0500

    Update smolyay/basis.py
    Co-authored-by: Michael Howard <mphoward@auburn.edu>

--- smolyay/basis.py
+++ smolyay/basis.py
@@ -575,7 +575,7 @@ class BasisFunctionSet(collections.abc.Sequence):
             y[i] = bf(new_X)
         return y
 
-    def derivative(self, X, n=1, domain=None):
+    def derivative(self, X, domain=None, n=1):
         """Evaluate all the derivative of basis functions in the set
 
         Calls the derivative for all the basis function(s) and
