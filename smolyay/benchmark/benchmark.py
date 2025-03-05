a5aac5094f5d0bf5ac44cea99c2ccce439a0c0ba
mzbush <107584654+mzbush@users.noreply.github.com>
2025-03-05 15:56:30 -0600

    Feature/benchmark update (#29)
    * Make benchmark parent compatible with point set
    
    * Apply black formatting
    
    * Add test that point set is a valid input

--- smolyay/benchmark/benchmark.py
+++ smolyay/benchmark/benchmark.py
@@ -1,6 +1,8 @@
 import abc
 
 import numpy
+from smolyay import samples
+
 
 class BenchmarkFunction(abc.ABC):
     """Benchmark Function
@@ -10,7 +12,8 @@ class BenchmarkFunction(abc.ABC):
     solutions exist or can be arbitrary.
 
     """
-    def __call__(self,x):
+
+    def __call__(self, x):
         """Evaluate the function.
 
         Parameters
@@ -25,21 +28,42 @@ class BenchmarkFunction(abc.ABC):
         ValueError
             If the input is outside the function domain.
         """
-        x = numpy.array(x, copy=False, ndmin=2)
-        if self.dimension == 1:
-            if x.ndim == 2 and x.shape[0] == 1 and x.shape[1]  > 1:
-                # the cast to 2d puts these in wrong order, so transpose
-                x = x.T
-            else:
-                x = x[..., numpy.newaxis]
-        if x.shape[-1] != self.dimension:
-            raise IndexError("Input must match dimension of domain")
-        if any(numpy.any(x[..., i] < self.domain[i][0]) or 
-               numpy.any(x[..., i] > self.domain[i][1]) 
-               for i in range(self.dimension)):
-            raise ValueError("Input outside domain of function.")
+        if isinstance(x, samples.UnidimensionalPointSet):
+            # input has a domain property, so just compare domain properties
+            if self.dimension > 1:
+                raise IndexError("Input must match dimension of domain")
+            if x.domain[0] < self.domain[0][0] or x.domain[1] > self.domain[0][1]:
+                raise ValueError("Input outside domain of function.")
+            x = x.points
+        elif isinstance(x, samples.MultidimensionalPointSet):
+            # input has a domain property, so just compare domain properties
+            if self.dimension != x.num_dimensions:
+                raise IndexError("Input must match dimension of domain")
+            if any(
+                x.domain[i][0] < self.domain[i][0] or x.domain[i][1] > self.domain[i][1]
+                for i in range(self.dimension)
+            ):
+                raise ValueError("Input outside domain of function.")
+            x = x.points
+        else:
+            # does not have a domain property, need to check all values are in domain
+            x = numpy.array(x, copy=None, ndmin=2)
+            if self.dimension == 1:
+                if x.ndim == 2 and x.shape[0] == 1 and x.shape[1] > 1:
+                    # the cast to 2d puts these in wrong order, so transpose
+                    x = x.T
+                else:
+                    x = x[..., numpy.newaxis]
+            if x.shape[-1] != self.dimension:
+                raise IndexError("Input must match dimension of domain")
+            if any(
+                numpy.any(x[..., i] < self.domain[i][0])
+                or numpy.any(x[..., i] > self.domain[i][1])
+                for i in range(self.dimension)
+            ):
+                raise ValueError("Input outside domain of function.")
         return numpy.squeeze(self._function(x))
-    
+
     @property
     def name(self):
         """str: Name of the function"""
@@ -54,7 +78,7 @@ class BenchmarkFunction(abc.ABC):
     def lower_bounds(self):
         """list: the lower bounds of the domain of each variable."""
         return [bound[0] for bound in self.domain]
-    
+
     @property
     def upper_bounds(self):
         """list: the upper bounds of the domain of each variable."""
@@ -64,14 +88,11 @@ class BenchmarkFunction(abc.ABC):
     @abc.abstractmethod
     def domain(self):
         """list: Domain of the function.
-        
+
         The domain must be specified as lower and upper bounds for each variable as a list of lists.
         """
         pass
- 
+
     @abc.abstractmethod
-    def _function(self,x):
+    def _function(self, x):
         pass
-
-
-
